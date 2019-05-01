34d32f598ce5ef1ec57513f59fb35334ca674e62
xekyle <xekyle@gmail.com>
2019-05-01 01:23:29 -0600

    installed dependenies and built entry point file.

--- app.js
+++ app.js
@@ -27,4 +27,45 @@ mongoose.connect(keys.mongoURI, {useNewUrlParser: true})
  .then(()=> console.log('Chattering...'))
  .catch(err => console.log(err));
 
- 
+const auth = require('./routes/auth');
+const index = require('./routes/index');
+const opinions = require('./routes/opinions');
+
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cookieParser());
+
+const sess = {
+  secret: 'greenRolledAndRolling',
+  name: 'puffPuffPass',
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  cookie: { path: '/', 
+            httpOnly: true,
+            secure: 'auto',
+            maxAge: 60000*60*24
+          }
+};
+app.use(session(sess));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
+  res.locals.user = req.user
+  next();
+});
+
+app.use('/', index);
+app.use('/auth', auth);
+app.use('/opinions', opinions);
+
+const port = process.env.PORT || 4200;
+
+app.listen(port, ()=>{ console.log(`Full steam at http://localhost:${port} & building at http://${ip.address()}!`)})
